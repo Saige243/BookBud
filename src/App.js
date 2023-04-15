d31349a6a25e7528c7ec3270ad792d31a9d9543c
Saige Cross <saigecrossdev@gmail.com>
2023-04-15 18:01:02 -0500

    Signup and sign in forms created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import ProtectedRoute from './components/ProtectedRoute';
+
 
 function App() {
+  const [currentUser, setCurrentUser] = useState()
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Dashboard />} />
+        <Route exact path="/" element={<ProtectedRoute currentUser={currentUser}><Dashboard /></ProtectedRoute>} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
       </Routes>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-function Login() {
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // handle login logic here
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="flex items-center justify-center h-screen">
+      <form onSubmit={handleSubmit} className="w-1/3">
+        <label htmlFor="email" className="text-gray-700 font-medium">
+          Email
+        </label>
+        <input
+          type="email"
+          placeholder="hello@example.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="block w-full px-4 py-2 mb-6 text-gray-700 border rounded shadow-sm focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+        />
+        <label htmlFor="password" className="text-gray-700 font-medium">
+          Password
+        </label>
+        <input
+          type="password"
+          placeholder="******"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="block w-full px-4 py-2 mb-16 text-gray-700 border rounded shadow-sm focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+        />
+        <button
+          type="submit"
+          className="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+        >
+          Sign In
+        </button>
+        <div className="text-gray-700 text-center pt-2">
+          Need an account?{' '}
+          <Link to="/signup" className="text-blue-500 font-medium">
+            Sign up here
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;

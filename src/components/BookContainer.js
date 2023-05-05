026f427992ca0575e6f8faa494a908220035d597
Saige Cross <saigecrossdev@gmail.com>
2023-05-05 09:51:51 -0500

    Book container finished

--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -1,25 +1,43 @@
 import React from 'react'
 import { PrimaryButton } from './Buttons'
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import InfoIcon from '@mui/icons-material/Info';
+import StarIcon from '@mui/icons-material/Star';
+
 
 function BookContainer({ props }) {
   const { volumeInfo } = props
 
   console.log('book img:', volumeInfo)
   return (
-    <div className='flex flex-row border-2 rounded-md w-96'>
-      <div className='w-20 h-28 mr-2'>
+    <div className='flex flex-row border-2 rounded-md w-96 m-2 justify-between'>
+      <div className='w-24 mr-2'>
         <img className='w-full h-full' src={volumeInfo?.imageLinks?.thumbnail} alt={volumeInfo.title} />
       </div>
-      <div className='flex flex-col'>
-        <h2>{volumeInfo.title}</h2>
+      <div className='flex flex-col text-left'>
+        <h2 className='font-bold'>{volumeInfo.title}</h2>
         <h3>{volumeInfo.authors}</h3>
-        <div className='flex flex-row '>
-          <PrimaryButton text='Save' size="small" />
-          <PrimaryButton text='Info' size="small" />
+        <div className='flex align-middle h-full'>
+          <p>
+            {volumeInfo.averageRating ? (
+              <>
+                <span className='flex items-center'>
+                  Rating: {volumeInfo.averageRating}
+                  <StarIcon fontSize='small' className='w-4 h-4 ml-1 text-yellow-500' />
+                </span>
+              </>
+            ) : (
+              'No ratings'
+            )}
+          </p>
 
         </div>
       </div>
-    </div>
+      <div className='flex flex-col align-middle justify-center'>
+        <PrimaryButton size="tiny" text={<FavoriteIcon />} />
+        <PrimaryButton size="tiny" text={<InfoIcon />} />
+      </div>
+    </div >
   )
 }
 

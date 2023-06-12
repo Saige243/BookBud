3488109081596b018c98af2563b1eb2bcb802c82
Saige Cross <saigecrossdev@gmail.com>
2023-06-12 17:57:32 -0500

    Restructured authContext, added getUser function

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
-import { useState, useContext, useEffect } from 'react';
-import { BrowserRouter, Routes, Route, redirect } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import SearchResults from './pages/SearchResults';
-import Login from './pages/Login';
-import ProtectedRoute from './components/ProtectedRoute';
-import NotFound from './pages/NotFound';
-import Navbar from './components/Navbar';
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import SearchResults from './pages/SearchResults'
+import Login from './pages/Login'
+import ProtectedRoute from './components/ProtectedRoute'
+import NotFound from './pages/NotFound'
+import Navbar from './components/Navbar'
 import Profile from './pages/Profile'
-import BookPage from './pages/BookPage';
-import { useGetBooks } from './hooks/useGetBooks';
+import BookPage from './pages/BookPage'
+import { useGetBooks } from './hooks/useGetBooks'
 import Signup from './pages/Signup'
-import useAuth from './auth/useAuth';
-import { AuthContext } from './auth/AuthContext';
+import { useAuthContext } from './hooks/useAuthContext'
 
 function App() {
-  const { isAuthenticated } = useContext(AuthContext);
-  const [searchTerm, setSearchTerm] = useState('');
-  const { books } = useGetBooks(searchTerm);
-  const [jwtToken, setJwtToken] = useState(localStorage.getItem('jwtToken'));
+  const { user } = useAuthContext()
+  const [searchTerm, setSearchTerm] = useState('')
+  const { books } = useGetBooks(searchTerm)
 
   const handleSearch = async (searchTerm) => {
-    setSearchTerm(searchTerm);
-  };
+    setSearchTerm(searchTerm)
+  }
 
-  console.log('AUTH?:', isAuthenticated)
+  console.log('Authenticated?:', user)
 
   return (
     <BrowserRouter>
-      {isAuthenticated && <Navbar onSubmitSearch={handleSearch} />}
+      {user && <Navbar onSubmitSearch={handleSearch} />}
       <Routes>
         <Route exact path="/" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
-        <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+        <Route element={<ProtectedRoute isAuthenticated={user} />}>
           <Route path="/dashboard" element={<Dashboard books={books} />} />
-          <Route path="/searchResults" element={<SearchResults books={books} searchTerm={searchTerm} />} />
+          <Route
+            path="/searchResults"
+            element={<SearchResults books={books} searchTerm={searchTerm} />}
+          />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/books/:bookId" element={<BookPage navigate={handleSearch} />} />
+          <Route
+            path="/books/:bookId"
+            element={<BookPage navigate={handleSearch} />}
+          />
         </Route>
         <Route exact path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { AuthProvider } from "./auth/AuthContext"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import AuthProvider from './auth/AuthProvider'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
   </React.StrictMode>
-);
+)
 
-reportWebVitals();
+reportWebVitals()

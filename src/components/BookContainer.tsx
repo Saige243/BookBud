859873812919218be243cfc5c5b9a7a5eae5ce5d
Saige Cross <saigecrossdev@gmail.com>
2023-09-03 14:47:01 -0500

    Responsiveness for mobile changes

--- src/components/BookContainer.tsx
+++ src/components/BookContainer.tsx
@@ -19,12 +19,12 @@ function BookContainer({ props }: { props: BookContainerProps }) {
   }
 
   return (
-    <div className="flex flex-col rounded-md m-3 justify-between hover:opacity-80 pb-16">
+    <div className="flex flex-col rounded-md md:m-3 justify-between hover:opacity-80 pb-16">
       <Link to={`/books/${props.id}`}>
-        <div className="flex flex-col justify-center w-36 mr-2">
-          <div className="relative h-60">
-            <div className="absolute h-60 w-36 opacity-0 hover:opacity-100">
-              <div className="relative left-[87px]">
+        <div className="flex flex-col justify-center w-24 md:w-36 mr-2">
+          <div className="relative h-40 md:h-60">
+            <div className="absolute h-60 w-20 opacity-0 hover:opacity-100">
+              <div className="relative md:left-[87px]">
                 <PrimaryButton
                   text={
                     <Modal modalText={<FavoriteIcon />} bookId={props.id} />
@@ -42,7 +42,7 @@ function BookContainer({ props }: { props: BookContainerProps }) {
               alt={volumeInfo.title}
             />
           </div>
-          <h2 className="font-bold font-unbounded font-lg truncate pt-2">
+          <h2 className="font-unbounded md:font-lg truncate pt-2">
             {volumeInfo.title}
           </h2>
           <h3 className="font-montserrat truncate">{volumeInfo.authors}</h3>
@@ -54,7 +54,9 @@ function BookContainer({ props }: { props: BookContainerProps }) {
               precision={0.5}
               readOnly
             />
-            <p className="pl-2 text-sm">{volumeInfo.averageRating}</p>
+            <p className="hidden sm:flex pl-2 text-sm">
+              {volumeInfo.averageRating}
+            </p>
           </div>
         </div>
       </Link>
